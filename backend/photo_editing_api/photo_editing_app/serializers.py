11872f67146219185129c344497f0bee134ecc63
Saad <ah12sa65@gmail.com>
2023-09-10 22:34:52 -0400

    Updated during Fall 2023

--- backend/photo_editing_api/photo_editing_app/serializers.py
+++
@@ -1,6 +0,0 @@
-# from rest_framework import serializers
-# from photo_editing_app.models import FunctionActivity
-# class FunctionActivitySerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = FunctionActivity
-#         fields = ("__all__")
