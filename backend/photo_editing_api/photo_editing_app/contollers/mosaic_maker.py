c8a59ea946d5031d2d0f3b06dcc4ce357de0851f
Saad <ah12sa65@gmail.com>
2023-10-15 18:40:47 -0400

    Fixed Mosaic Tool

--- backend/photo_editing_api/photo_editing_app/contollers/mosaic_maker.py
+++ backend/photo_editing_api/photo_editing_app/contollers/mosaic_maker.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageOps
+from PIL import Image, ImageOps, ImageFilter
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import image
@@ -14,7 +14,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 #def create_mosaic(main_image, folder_path, num_images):
-def mosaicmaker(input_path, output_path, images_list, num_images):
+def mosaicmaker(input_path, output_path, images_list, pixel_size):
 #def mosaicmaker(input_path, output_path, folder_path, num_images):
 
     main_image = input_path
@@ -32,7 +32,7 @@ def mosaicmaker(input_path, output_path, images_list, num_images):
         return im_arr
 
     # Load the main image
-    plt.figure(figsize=(8, 8))
+    
     face_im_arr = load_image(main_image)
     face_image = Image.fromarray(face_im_arr)
     # plt.imshow(face_image)
@@ -42,71 +42,36 @@ def mosaicmaker(input_path, output_path, images_list, num_images):
     width = face_im_arr.shape[0]
     height = face_im_arr.shape[1]
 
-    # Set the target resolution
-    target_res = (100, 100)
+    num_iterations_width = width // pixel_size + (1 if width % pixel_size != 0 else 0)
+    num_iterations_height = height // pixel_size + (1 if height % pixel_size != 0 else 0)    
 
-    # Create a template for mosaic
-    plt.figure(figsize=(8, 8))
-    mos_template = face_im_arr[::(height // target_res[0]), ::(height // target_res[1])]
-    # plt.imshow(Image.fromarray(mos_template))
-    # plt.show()
 
-    # Number of images required to fill mosaic
-    num_mosaic_images = mos_template[:, :, -1].size
+    resized_images = {}
 
-    logger.info("creating list of images as np arrays")
-    # Create a list of all images as np arrays
-    images = []
+    # Loop through each image in the list and resize it
     for file in images_list:
-    #for file in glob.glob(os.path.join(folder_path, '*')):
-        im = load_image(file)
-        images.append(im)
-
-    # Delete any images with unexpected shapes
-    images = [i for i in images if i.ndim == 3]
-
-    # Resize the images
-    def resize_image(img: Image, size: tuple) -> np.ndarray:
-        resz_img = ImageOps.fit(img, size, Image.ANTIALIAS, centering=(0.5, 0.5))
-        resz_img = resz_img.convert('RGB')
-        return np.array(resz_img)
-
-    logger.info("Resizing images")
-    mosaic_size = (40, 40)
-    images = [resize_image(Image.fromarray(i), mosaic_size) for i in images]
+        img =  Image.open(file)
+        resized_images[file] = img.resize((pixel_size, pixel_size), Image.LANCZOS)
 
 
-    # Convert list to np array
-    images_array = np.asarray(images)
+    mosaic = Image.new('RGB', (width, height))
 
-    logger.info("get mean of RGB values")
-    # Get mean of RGB values for each image
-    image_values = np.apply_over_axes(np.mean, images_array, [1, 2]).reshape(len(images), 3)
+    for i in range(num_iterations_width):
+        for j in range(num_iterations_height):
+            random_image_path = random.choice(images_list)
 
-    logger.info("set KD tree")
-    # Set KDTree for image_values
-    tree = spatial.KDTree(image_values)
+            random_image = resized_images[random_image_path]
 
-    logger.info("Storing indices of best images")
-    # Store the indices of best match images
-    image_idx = np.zeros(target_res, dtype=np.uint32)
-    k = int(num_images / 3)
-    if (k > 40):
-        k = 5
-    for i in range(target_res[0]):
-        for j in range(target_res[1]):
-            template = mos_template[i, j]
-            match = tree.query(template, k)
-            pick = random.randint(0, k - 1)
-            image_idx[i, j] = match[1][pick]
+            paste_x = i * pixel_size
+            paste_y = j * pixel_size
 
-    # Loop through the best match indices, retrieve the matching image and add it to the canvas
-    canvas = Image.new('RGB', (mosaic_size[0] * target_res[0], mosaic_size[1] * target_res[1]))
-    for i in range(target_res[0]):
-        for j in range(target_res[1]):
-            arr = images[image_idx[j, i]]
-            x, y = i * mosaic_size[0], j * mosaic_size[1]
-            im = Image.fromarray(arr)
-            canvas.paste(im, (x, y))
+            # Ensure the paste position does not exceed the dimensions of the main image
+            paste_x = min(paste_x, width - pixel_size)
+            paste_y = min(paste_y, height - pixel_size)
+
+            mosaic.paste(random_image, (paste_x, paste_y))        
+    
+    #mosaic.paste(face_image, (0, 0))
+    canvas = Image.blend(face_image, mosaic, 0.5)
 
     canvas.save(output_path)
