254546c96232fe51f1d974ae47ec4697b52c5457
ayesha24 <ayesh.h1224@gmail.com>
2023-11-10 06:16:16 -0500

    image compression logic moved to here

--- backend/photo_editing_api/photo_editing_app/contollers/image_compression.py
+++ backend/photo_editing_api/photo_editing_app/contollers/image_compression.py
@@ -1,39 +1,29 @@
 import os
 import base64
 import cv2
+import logging
 
-def compress_image(input_image_path, output_image_path):
+logging.basicConfig(format= '[%(asctime)s] %(message)s')
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+def compress_image(output_format, image, compression_param):
+
+    logger.info("image compression in progress")
     try:
-        image = cv2.imread(input_image_path)
-        compression_params = [cv2.IMWRITE_JPEG_QUALITY, 10]
-        success = cv2.imwrite(output_image_path, image, compression_params)
+       
+        logger.info("starting compression")
+        # Compress the image
+        _, compressed_image = cv2.imencode("." + output_format, image, compression_param)
 
-        if not success:
-            raise Exception("Failed compressing the image file")
+        # Convert the compressed image to base64 for easy transmission
+        compressed_image_base64 = base64.b64encode(compressed_image.tobytes()).decode('utf-8')
         
-        input_file_size = "{:.2f}".format(os.path.getsize(input_image_path) / (1024 * 1024))
-        output_file_size = "{:.2f}".format(os.path.getsize(output_image_path) / (1024 * 1024))
-
-        with open(output_image_path, 'rb') as f:
-            output_image_data = f.read()
-        image_base64 = base64.b64encode(output_image_data).decode('utf-8')
-
-        response = {
-            'image_base64': f"data:image/jpeg;base64,{image_base64}",
-            'input_file_size': input_file_size + "MB",
-            'output_file_size': output_file_size + "MB",
-            'error': False,
-            'message': "Successfully compressed the image",
-            'status': 200
-        }
-
     except Exception as e:
-        response = {
-            'error': True,
-            'message': str(e),
-            'status': 400
-        }
+        logger.error("Error occured in compressing image" + str(e))
+        
 
-    return response
+    logger.info("image compression complete")
+    return compressed_image_base64
 
 
