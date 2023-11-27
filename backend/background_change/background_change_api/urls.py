66db6868c9712ac785dcaa27146d6eb2c730198d
PavanaL <pavana91@gmail.com>
2023-11-26 20:40:13 -0500

    background change microservice

---
+++ backend/background_change/background_change_api/urls.py
@@ -0,0 +1,13 @@
+
+from django.contrib import admin
+from django.urls import path
+from background_change_app import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    path('background_change/', views.background_change, name='background_change'),
+    
+    path('stats/', views.get_db_stat, name='stats'),
+    
+]   
