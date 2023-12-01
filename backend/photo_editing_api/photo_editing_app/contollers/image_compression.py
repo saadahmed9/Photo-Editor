df8bfe83edf6d5d418f3ccd72f2da3089d596b5f
ayesha24 <ayesh.h1224@gmail.com>
2023-12-01 18:46:17 -0500

    change of img compression logic

--- backend/photo_editing_api/photo_editing_app/contollers/image_compression.py
+++ backend/photo_editing_api/photo_editing_app/contollers/image_compression.py
@@ -2,28 +2,29 @@ import os
 import base64
 import cv2
 import logging
+from PIL import Image
+import io
 
 logging.basicConfig(format= '[%(asctime)s] %(message)s')
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-def compress_image(output_format, image, compression_param):
+class ImageCompressClass():
+    def compress_image(output_format, image, compression_param):
 
-    logger.info("image compression in progress")
-    try:
-       
-        logger.info("starting compression")
-        # Compress the image
-        _, compressed_image = cv2.imencode("." + output_format, image, compression_param)
-
-        # Convert the compressed image to base64 for easy transmission
-        compressed_image_base64 = base64.b64encode(compressed_image.tobytes()).decode('utf-8')
+        logger.info("image compression method in progress")
+        try:
         
-    except Exception as e:
-        logger.error("Error occured in compressing image" + str(e))
-        
+            logger.info("starting compression")
+            # Compress the image
+            _, compressed_image = cv2.imencode("." + output_format, image, compression_param)
 
-    logger.info("image compression complete")
-    return compressed_image_base64
-
-
+            # Convert the compressed image to base64 for easy transmission
+            compressed_image_base64 = base64.b64encode(compressed_image.tobytes()).decode('utf-8')
+            
+        except Exception as e:
+            logger.error("Error occured in compressing image" + str(e))
+            
+        logger.info("image compression method complete")
+        return compressed_image_base64
+    
