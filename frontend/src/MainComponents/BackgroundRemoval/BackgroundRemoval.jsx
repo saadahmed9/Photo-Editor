ddbf78805b8efb02088aa9918217b60e0f19ad2a
Saad <ah12sa65@gmail.com>
2023-12-01 15:37:51 -0500

    Updated URL for bg change

--- frontend/src/MainComponents/BackgroundRemoval/BackgroundRemoval.jsx
+++ frontend/src/MainComponents/BackgroundRemoval/BackgroundRemoval.jsx
@@ -162,7 +162,9 @@ function BackgroundRemoval (props1) {
     formData.append('myfile', dataURLtoFile(imageUrl,fileName+"."+fileType));
     formData.append('background_change', color);
     formData.append('function', 'background_change');
-    axios.post(process.env.REACT_APP_BACKGROUND_CHANGE_API_URL+'/background_change/', formData)
+    //axios.post(process.env.REACT_APP_BACKGROUND_CHANGE_API_URL+'/background_change/', formData)
+    axios.post("http://xlabk8s3.cse.buffalo.edu:30018/background_change/", formData)
+    
       .then(response => {
         downloadImage(response.data.imageUrl)
       }
