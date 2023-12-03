bda7f79329fac7023cedade434bb63bdd4f64aa1
ayesha24 <32022409+ayesha24@users.noreply.github.com>
2023-12-03 18:58:58 -0500

    Update views.py - Implemented custom compression
    Change of logic in Compression rate.
    Custom Image compression also implemented.

--- backend/image_compression/image_compression_app/views.py
+++ backend/image_compression/image_compression_app/views.py
@@ -13,7 +13,7 @@ import logging
 
 import cv2
 import numpy as np
-from image_compression_app.contollers.image_compression import compress_image
+from image_compression_app.contollers.image_compression import ImageCompressClass as ICC
 # Create your views here.
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -90,41 +90,83 @@ def image_compression(request):
     logger.info("image compression endpoint!")
     return_dict = {}
     if request.method == 'POST':
+        logger.info("entered POST!")
         try:
             verify_upload_file_passed(request)
             verify_functionality_passed(request)
             image_file = request.FILES.get('myfile')
             image_file.name = image_file.name.replace(" ", "")
             compression_rate = int(request.POST.get('compression_rate'))
+            compression_rate = random.randint(10, 25)
+            target_size = request.POST.get('custom_rate')
+            input_image_size = image_file.size
+            input_image_size_temp = image_file.size
+            logger.info("verification done")
+            target_size = request.POST.get('custom_rate')
             logger.info(compression_rate)
+            logger.info(target_size)
             output_format = request.POST.get('output_format')
+
             # Read the image using OpenCV
             image = cv2.imdecode(np.frombuffer(image_file.read(), np.uint8), 1)
 
+            if target_size == 'null':
             # Determine compression parameter based on output format
-            compression_param = None
-            if output_format.lower() == 'jpeg':
-                compression_param = [cv2.IMWRITE_JPEG_QUALITY, compression_rate]
-            elif output_format.lower() == 'jpg':
-                compression_param = [cv2.IMWRITE_JPEG_QUALITY, compression_rate]
-            elif output_format.lower() == 'png':
-                compression_param = [cv2.IMWRITE_PNG_COMPRESSION, compression_rate]
-            elif output_format.lower() == 'tiff':
-                compression_param = [cv2.IMWRITE_TIFF_COMPRESSION, compression_rate]
+                compression_param = None
+                if output_format.lower() == 'jpeg':
+                    compression_param = [cv2.IMWRITE_JPEG_QUALITY, compression_rate]
+                elif output_format.lower() == 'jpg':
+                    compression_param = [cv2.IMWRITE_JPEG_QUALITY, compression_rate]
+                elif output_format.lower() == 'png':
+                    compression_param = [cv2.IMWRITE_PNG_COMPRESSION, compression_rate]
+                elif output_format.lower() == 'tiff':
+                    compression_param = [cv2.IMWRITE_TIFF_COMPRESSION, compression_rate]
 
-            if compression_param:
-                compressed_image_base64 = compress_image(output_format, image, compression_param)
+                if compression_param:
+                    compressed_image_base64 = ICC.compress_image(output_format, image, compression_param)
+                    compressed_image_bytes = base64.b64decode(compressed_image_base64)
+                    output_image_size = sys.getsizeof(compressed_image_bytes)
+                    # Add relevant information to the return dictionary
+                    return_dict['imageUrl'] = f"data:image/{output_format};base64,{compressed_image_base64}"
+                    return_dict['input_image_size'] = f"{math.ceil(input_image_size/1024)} KB"
+                    return_dict['output_image_size'] = f"{math.ceil(output_image_size/1024)} KB"
+                    return_dict['format'] = output_format
+                    return_dict['error'] = False
+                    return_dict['message'] = "Successfully processed image"
+                    return_dict['status'] = 200
+                else:
+                    return_dict['error'] = True
+                    return_dict['message'] = 'Unsupported output format'
+                    return_dict['status'] = 400
+            
+            elif target_size != 'null':
+                target_size_temp = int(target_size)
+                logger.info("custom compression under progress")
+                compression_param = None
+                compression_rate = 100
+                
+                while (input_image_size_temp/1024) > target_size_temp and compression_rate > 10:
+                    logger.info("enetred while loop")
+                    compression_rate -= 10
+                    if output_format.lower() == 'jpeg' or output_format.lower() == 'jpg':
+                        compression_param = [cv2.IMWRITE_JPEG_QUALITY, compression_rate]
+                    elif output_format.lower() == 'png':
+                        compression_param = [cv2.IMWRITE_PNG_COMPRESSION, compression_rate]
+                    elif output_format.lower() == 'tiff':
+                        compression_param = [cv2.IMWRITE_TIFF_COMPRESSION, compression_rate]
+                    compressed_image_base64 = ICC.compress_image(output_format, image, compression_param)
+                    input_image_size_temp = len(compressed_image_base64)
 
-                # Add relevant information to the return dictionary
+                compressed_image_bytes = base64.b64decode(compressed_image_base64)
+                output_image_size = sys.getsizeof(compressed_image_bytes)
+                logger.info("custom compression completed in views")
                 return_dict['imageUrl'] = f"data:image/{output_format};base64,{compressed_image_base64}"
+                return_dict['input_image_size'] = f"{math.ceil(input_image_size/1024)} KB"
+                return_dict['output_image_size'] = f"{math.ceil(output_image_size/1024)} KB"
                 return_dict['format'] = output_format
                 return_dict['error'] = False
                 return_dict['message'] = "Successfully processed image"
                 return_dict['status'] = 200
-            else:
-                return_dict['error'] = True
-                return_dict['message'] = 'Unsupported output format'
-                return_dict['status'] = 400
 
         except Exception as e:
             return_dict['error'] = True
