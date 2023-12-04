f34a51755304d218fa84948543d745ac257a66b5
PavanaL <pavana91@gmail.com>
2023-12-03 23:04:02 -0500

    update cron job to del file older than 1 min

--- backend/passport-service/photo_editing_app/contollers/cron1.py
+++ backend/passport-service/photo_editing_app/contollers/cron1.py
@@ -13,7 +13,7 @@ class RemoveFiles():
             last_folder_update_datetime = datetime.datetime.fromtimestamp(last_forder_update_timestamp)
             current_datetime = datetime.datetime.now()
             difference = current_datetime - last_folder_update_datetime
-            if difference.total_seconds()/60 > 10:
+            if difference.total_seconds()/60 > 1:
                 os.remove(image_path)
 
         path1 = "media/uploads"
@@ -24,18 +24,7 @@ class RemoveFiles():
             last_folder_update_datetime = datetime.datetime.fromtimestamp(last_forder_update_timestamp)
             current_datetime = datetime.datetime.now()
             difference = current_datetime - last_folder_update_datetime
-            if difference.total_seconds() / 60 > 10:
-                os.remove(image_path)
-
-        path1 = "media/Mosaic_input"
-        files1 = os.listdir(path1)
-        for file in files1:
-            image_path = path1 + file
-            last_forder_update_timestamp = os.stat(image_path).st_mtime
-            last_folder_update_datetime = datetime.datetime.fromtimestamp(last_forder_update_timestamp)
-            current_datetime = datetime.datetime.now()
-            difference = current_datetime - last_folder_update_datetime
-            if difference.total_seconds() / 60 > 10:
+            if difference.total_seconds() / 60 > 1:
                 os.remove(image_path)
 
 class MyCronJob(CronJobBase):
