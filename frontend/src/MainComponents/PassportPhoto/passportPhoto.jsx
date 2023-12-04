1cb9549c3cd2c2936791b412a7c9d4c7aa9d61cc
Taraka Rohit Adusumilli <tarakarohit@gmail.com>
2023-12-04 09:24:21 -0500

    Revert "Correct URL Pointing"
    This reverts commit ad79f0115d839e09995405313c1c5c315b0958c3.

--- frontend/src/MainComponents/PassportPhoto/passportPhoto.jsx
+++ frontend/src/MainComponents/PassportPhoto/passportPhoto.jsx
@@ -442,7 +442,7 @@ const PassportPhoto = (props1) => {
         formData.append('background_req', 'no');
       }
       console.log(process.env.REACT_APP_PASSPORT_API_URL)
-    //axios.post(process.env.REACT_APP_API_URL+'/passport_photo_size/', formData)
+    //  axios.post(process.env.REACT_APP_API_URL+'/passport_photo_size/', formData)
       axios.post("http://xlabk8s3.cse.buffalo.edu:30010/passport_photo_size/", formData)
     
       .then(response => {
